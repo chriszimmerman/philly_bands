40b9ab4afd101c797a60f8c379aaaa0a1ed2e5b7
Chris Zimmerman <chris@chriszimmerman.net>
2026-02-08 13:41:51 -0500

    Update oban

--- priv/repo/migrations/20260208183518_add_oban_jobs_table.exs
+++ priv/repo/migrations/20260208183518_add_oban_jobs_table.exs
@@ -2,10 +2,10 @@ defmodule PhillyBands.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migration.up(version: 1)
+    Oban.Migration.up()
   end
 
   def down do
-    Oban.Migration.down(version: 1)
+    Oban.Migration.down()
   end
 end
