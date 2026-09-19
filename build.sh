b956f68452ec66e298c36ad54faeaa198a01e239
Chris Zimmerman <chris@chriszimmerman.net>
2026-09-19 18:57:17 -0400

    Revert change

--- build.sh
+++ build.sh
@@ -18,4 +18,3 @@ MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite
 
 _build/prod/rel/philly_bands/bin/philly_bands eval "PhillyBands.Release.migrate"
-_build/prod/rel/philly_bands/bin/philly_bands eval "PhillyBands.Events.FetchJob.run"
