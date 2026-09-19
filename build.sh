1f56173a2f6c55dd9cf4ebe656013d910e62e817
Chris Zimmerman <chris@chriszimmerman.net>
2026-09-19 18:50:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,3 +16,5 @@ MIX_ENV=prod mix assets.deploy
 # Create server script, Build the release, and overwrite the existing release directory
 MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite
+
+_build/prod/rel/<APP_NAME>/bin/<APP_NAME> eval "<MODULE_NAME>.Release.migrate"
